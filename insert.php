ca802e0e6b2a9bbf9deb4b834c5cf2fcc992de65
Alex José Silva <turmawb@gmail.com>
2017-08-31 13:27:43 -0300

    conexao bd
    conexao bd

--- insert.php
+++ insert.php
@@ -1,13 +1,12 @@
  <?php	
 
-	$HOST  = "//127.0.0.1:55609/";
+	$HOST  = "127.0.0.1:55609";
 	$LOGIN = "azure";
-	$SENHA = "vWHD_";
-	$dbName= 'localdb';
+	$SENHA = "6#vWHD_$";
+	$db = "localdb";
  
 	mysql_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
-	
-	mysql_select_db($dbName) or die("Não foi possível selecionar o banco de dados");
+	mysql_select_db($db) or die("Não foi possível SELECIONAR o banco de dados");
 	
 	$data		= json_decode(file_get_contents("php://input"));
 	$name 		= mysql_real_escape_string($data->name);
