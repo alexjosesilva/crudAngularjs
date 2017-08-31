20832c1fbb94630dd61bd8d7601dd43316e1af69
Alex José Silva <turmawb@gmail.com>
2017-08-31 16:55:36 -0300

    rte
    tert

--- insert.php
+++ insert.php
@@ -1,14 +1,14 @@
  <?php	
 
-	$HOST  = "127.0.0.1:55609";
-	$LOGIN = "azure";
-	$SENHA = "6#vWHD_$";
-	$db = "localdb";
+	$HOST  	= "127.0.0.1:55609";
+	$LOGIN 	= "azure";
+	$SENHA 	= "6#vWHD_$";
+	$db 	= "localdb";
  
 	mysql_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
 	mysql_select_db($db) or die("Não foi possível SELECIONAR o banco de dados");
 	
-	$data	= json_decode(file_get_contents("php://input"));
+	$data		= json_decode(file_get_contents("php://input"));
 	$id 		= $data->id;
 	$name 		= $data->name;
 	$price 		= $data->price;
@@ -16,7 +16,7 @@
 	
 	$erro = 'arquivo1.txt';
 	$file = fopen($erro, 'a');
-	$texto = $db;
+	$texto = $data->name;
 	fwrite($file, $texto);
 	fclose($file);
 	 
