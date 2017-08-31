680273c1bb8eb13b22b49957e7faa6db99a8062a
Alex José Silva <turmawb@gmail.com>
2017-08-31 12:19:50 -0300

    configurar banco
    configurar banco

--- insert.php
+++ insert.php
@@ -1,10 +1,13 @@
  <?php	
-	$HOST  = "localhost";
-	$LOGIN = "root";
-	$SENHA = "";
+
+	$HOST  = "//127.0.0.1:55609/";
+	$LOGIN = "azure";
+	$SENHA = "vWHD_";
+	$dbName= 'localdb';
  
 	mysqli_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
-	mysqli_select_db("bancoteste") or die("Não foi possível selecionar o banco de dados");
+	
+	mysqli_select_db($dbName) or die("Não foi possível selecionar o banco de dados");
 	
 	$data		= json_decode(file_get_contents("php://input"));
 	$name 		= mysqli_real_escape_string($data->name);
