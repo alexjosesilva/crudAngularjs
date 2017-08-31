0a0be62fb55468ffe51c2885873c3bf6b5239a70
Alex José Silva <turmawb@gmail.com>
2017-08-31 13:24:52 -0300

    db
    db

--- insert.php
+++ insert.php
@@ -5,18 +5,18 @@
 	$SENHA = "vWHD_";
 	$dbName= 'localdb';
  
-	mysqli_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
+	mysql_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
 	
-	mysqli_select_db($dbName) or die("Não foi possível selecionar o banco de dados");
+	mysql_select_db($dbName) or die("Não foi possível selecionar o banco de dados");
 	
 	$data		= json_decode(file_get_contents("php://input"));
-	$name 		= mysqli_real_escape_string($data->name);
-	$price 		= mysqli_real_escape_string($data->price);
-	$quantity 	= mysqli_real_escape_string($data->quantity);
+	$name 		= mysql_real_escape_string($data->name);
+	$price 		= mysql_real_escape_string($data->price);
+	$quantity 	= mysql_real_escape_string($data->quantity);
 		
-	$sql = "INSERT INTO tprodutos('id','name', 'price', 'quantity') VALUES(null,'".$name."','".$price."','".$quantity."')";
-	$result = mysqli_query($sql);
+	$sql = "INSERT INTO tprodutos('id','name', 'price', 'quantity') VALUES('".$name."','".$price."','".$quantity."')";
+	$result = mysql_query($sql);
 	
 	
-	mysqli_close();
+	mysql_close();
 ?>
