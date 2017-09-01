12dba0920c5a00a948b3ef1a567985f3b5449c7f
Alex José Silva <turmawb@gmail.com>
2017-09-01 13:01:42 -0300

    ew5ew5
    wewe

--- del.php
+++ del.php
@@ -1,15 +1,15 @@
  <?php	
 
-	$HOST  = "127.0.0.1:55609";
-	$LOGIN = "azure";
-	$SENHA = "6#vWHD_$";
-	$db = "localdb";
+	$HOST  	= "127.0.0.1:55609";
+	$LOGIN 	= "azure";
+	$SENHA 	= "6#vWHD_$";
+	$db 	= "localdb";
  
 	mysql_connect($HOST, $LOGIN, $SENHA) or die("Não foi possível a conexão com o servidor");
 	mysql_select_db($db) or die("Não foi possível SELECIONAR o banco de dados");
 	
-	$data		= json_decode(file_get_contents("php://input"));
-	$id			= mysql_real_escape_string($data->id);
+	$data	= json_decode(file_get_contents("php://input"));
+	$id		= mysql_real_escape_string($data->id);
 	
 
 	//arquivo de erros
@@ -23,7 +23,6 @@
 	//deletar	
 	$sql = "DELETE FROM `tprodutos` WHERE `id` =".$id;
 	$result = mysql_query($sql) or die("Não foi possível apagar o dados no banco");
-	
-	
+		
 	mysql_close();
 ?>
